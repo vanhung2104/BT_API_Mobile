c2843c8e60fedd4a7692e2c97b0eb60092ee676a
Hùng Nguyễn <vanhung211104@gmail.com>
2025-02-20 08:29:56 +0700

    Khởi tạo

---
+++ src/main/java/ute/bt_api_mobile/BtApiMobileApplication.java
@@ -0,0 +1,13 @@
+package ute.bt_api_mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtApiMobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtApiMobileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ute/bt_api_mobile/BtApiMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package ute.bt_api_mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtApiMobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
